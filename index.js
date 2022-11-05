fcc9a5d2e8d4204a0077baf5926e28cc68630497
Godfrey <kellemanatser@gmail.com>
2022-11-05 17:45:45 +0100

    zuri task

---
+++ index.js
@@ -0,0 +1,84 @@
+const express = require("express");
+const { json } = require("express");
+const flights = require("./controllers/flightController");
+const models = require("./models/Flight");
+const routes = require("./routes/flightRoute");
+
+const app = express();
+app.use(json());
+
+app.use("/", routes);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+let flight =[
+  {
+      id: 1,
+      title: 'flight to canada',
+      time: '1pm',
+      price: 26000,
+      date: '26-06-20'
+  }
+  ]
+  
+  // View all
+  app.get('/flight',(req, res)=>{
+      res.send(flight)
+      
+  })
+  
+  // search for one
+  app.get('/flight/:id', (req, res) => {
+      const ticket = flight.find(c => c.id === parseInt(req.params.id))
+      if (!ticket) res.status(404).send('Ticket not found')
+      res.send(ticket)
+  })
+  
+  // Add
+  app.post('/flight', (req, res) => {
+      const ticket = {
+          id: flight.length + 1,
+          title: req.body.title,
+          time: req.body.time,
+          price: req.body.price,
+          date: req.body.date
+      };
+      flight.push(ticket)
+      res.send(ticket);
+      
+   })
+  
+  
+   // update
+  app.put('/flight/:id', (req, res) => {
+      var id = req.params.id
+      var title = req.body.title
+      var time = req.body.time
+      var price = req.body.price
+      var date = req.body.date
+  
+          var index = flight.findIndex(el => el.id == id)
+          flight[index] = {
+              ...flight[index],
+              title: title,
+              time: time,
+              price: price,
+              date: date
+          }
+          res.send({
+              success: true,
+              message: 'Data updated successfully'
+          })
+  })
+  
+  // Delete
+  app.delete('/flight/:id', (req, res) => {
+      const ticket = flight.find(c => c.id === parseInt(req.params.id))
+      if (!ticket) res.status(404).send('Ticket not found')
+      const index = flight.indexOf(ticket)
+      flight.splice(index, 1);
+      res.send("Deleted successfully")
+      
+  })
